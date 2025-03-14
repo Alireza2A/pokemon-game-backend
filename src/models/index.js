b8942fe8996b0d75da11d0d8db749af8e16c8918
Julia Baur <juliabaur1@gmx.de>
2025-03-14 17:55:59 +0100

    added hardcoded Database url to index.js to connect to neon db

--- src/models/index.js
+++ src/models/index.js
@@ -1,12 +1,21 @@
 import { Sequelize } from 'sequelize';
 
-// Connect to Neon database
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+// Test with hardcoded DATABASE_URL
+const sequelize = new Sequelize('postgresql://PokemonBattleGame_owner:npg_L2eljRF7YsMg@ep-damp-forest-a2e5li68-pooler.eu-central-1.aws.neon.tech/PokemonBattleGame?sslmode=require', {
   dialect: 'postgres',
   ssl: {
     rejectUnauthorized: false,
   },
 });
+
+// Connect to Neon database (usual way, did not work properly, put the hardcoded db url instead)
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+// dialect: 'postgres',
+// ssl: {
+// rejectUnauthorized: false,
+// },
+// });
+
 
 const connectDB = async () => {
   try {
--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
-import dotenv from 'dotenv';
 import { connectDB } from './models/index.js';  
 import { sequelize } from './models/index.js';
-import app from './app.js'; // App importieren
+import app from './app.js'; 
+import dotenv from 'dotenv';
 
+// Lade Umgebungsvariablen
 dotenv.config();
 
 console.log('Database URL:', process.env.DATABASE_URL);
@@ -10,13 +11,18 @@ console.log('Database URL:', process.env.DATABASE_URL);
 const PORT = process.env.PORT || 5001;
 
 // Connect to database
-connectDB();
+connectDB().then(() => {
+  // Synchronisiere Modelle mit der Datenbank nach erfolgreicher Verbindung
+  sequelize.sync().then(() => {
+    console.log('Database synchronized!');
 
-// Synchronise model with DB
-sequelize.sync().then(() => {
-  console.log('Database synchronized!');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    // Starte den Server
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  }).catch((error) => {
+    console.error('Error syncing database:', error);
+  });
+}).catch((error) => {
+  console.error('Error connecting to the database:', error);
 });

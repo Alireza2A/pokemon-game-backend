a12986959a89312c4f9f1dff993b8bb212e22342
Julia Baur <juliabaur1@gmx.de>
2025-03-17 15:57:01 +0100

    installed cors and incorporated middleware to prepare communication with frontend

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ import { connectDB } from './models/index.js';
 import { sequelize } from './models/index.js';
 import app from './app.js'; 
 import dotenv from 'dotenv';
-
+import cors from 'cors';
+import helmet from 'helmet';
 
 dotenv.config();
 
@@ -10,9 +11,19 @@ console.log('Database URL:', process.env.DATABASE_URL);
 
 const PORT = process.env.PORT || 5001;
 
+// Cors configuration
+const corsOptions = {
+  origin: 'http://localhost:3000', // URL of frontend app
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+// Activate Cors and safety middleware
+app.use(cors(corsOptions));
+app.use(helmet());
+
 // Connect to database
 connectDB().then(() => {
-  
   sequelize.sync().then(() => {
     console.log('Database synchronized!');
 
@@ -24,4 +35,10 @@ connectDB().then(() => {
   });
 }).catch((error) => {
   console.error('Error connecting to the database:', error);
+});
+
+// simple error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
 });

608b0b39917d3a21623419a74291bbaf11440c88
Julia Baur <juliabaur1@gmx.de>
2025-03-18 11:46:18 +0100

    Improve api access security with helmet and CORS configuration in server.js

--- src/server.js
+++ src/server.js
@@ -11,18 +11,32 @@ console.log('Database URL:', process.env.DATABASE_URL);
 
 const PORT = process.env.PORT || 5001;
 
-// Cors configuration
+// List of allowed origins (URLs)
+const allowedOrigins = [
+  'http://localhost:3000',   // Local development server
+  'https://yourProductionDomain.com',  // Example for a production domain
+];
+
+// CORS configuration
 const corsOptions = {
-  origin: 'http://localhost:3000', // URL of frontend app
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      // If the origin is in the allowed list or if no origin is provided (e.g., for server-side requests)
+      callback(null, true);
+    } else {
+      // If the origin is not allowed, return an error
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
-// Activate Cors and safety middleware
-app.use(cors(corsOptions));
-app.use(helmet());
+// Activate CORS and security middleware
+app.use(cors(corsOptions));  // Use the configured CORS options
+app.use(helmet());           // Apply Helmet for enhanced security
 
-// Connect to database
+// Connect to the database
 connectDB().then(() => {
   sequelize.sync().then(() => {
     console.log('Database synchronized!');
@@ -37,7 +51,7 @@ connectDB().then(() => {
   console.error('Error connecting to the database:', error);
 });
 
-// simple error handling
+// Simple error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Something went wrong!' });
